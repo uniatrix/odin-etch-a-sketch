a96d61da1dae067ee0fcfdb94e4b8aa8177741a5
Cesar Seabra <uniatrix@gmail.com>
2023-12-18 17:16:37 -0300

    Added onclick functionality for drawing and some little polish.

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let color = "black";
+let isMouseDown = false;
 
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(16);
+
+  document.querySelector("body").addEventListener("mousedown", function (e) {
+    if (e.target.tagName != "BUTTON") {
+      isMouseDown = true;
+    }
+  });
+
+  document.querySelector("body").addEventListener("mouseup", function (e) {
+    if (e.target.tagName != "BUTTON") {
+      isMouseDown = false;
+    }
+  });
 
   let btnPopup = document.querySelector("#popup");
   btnPopup.addEventListener("click", function () {
@@ -26,22 +39,23 @@ function createBoard(size) {
 
 function getSize() {
   let input = prompt("Input board size:");
-  let message = document.querySelector("#message");
   if (input == "") {
     message.innerHTML = "Please provide a number";
   } else if (input < 0 || input > 100) {
     message.innerHTML = "Please provide a number between 1 and 100";
   } else {
-    message.innerHTML = "Let's start!";
+    message.innerHTML = "Board size changed!";
     return input;
   }
 }
 
 function colorDiv() {
-  if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-    this.style.backgroundColor = "black";
+  if (isMouseDown) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = "black";
+    }
   }
 }
 
