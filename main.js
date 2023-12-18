5b081faff257d051a8e230402b49f2f32b1de171
Cesar Seabra <uniatrix@gmail.com>
2023-12-18 16:52:05 -0300

    Implemented button functionality

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(16);
+
+  let btnPopup = document.querySelector("#popup");
+  btnPopup.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -11,7 +19,37 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "yellow";
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
+}
+
+function getSize() {
+  let input = prompt("Input board size:");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Please provide a number between 1 and 100";
+  } else {
+    message.innerHTML = "Let's start!";
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let allDivs = document.querySelectorAll("div");
+  allDivs.forEach((div) => (div.style.backgroundColor = "white"));
 }
